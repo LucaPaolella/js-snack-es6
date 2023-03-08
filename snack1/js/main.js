0e5f5ade0540ac6697e5c3a7db01ea730229bd52
Your Name <you@example.com>
2023-03-08 15:07:53 +0100

    Lista ospiti

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -1,2 +1,55 @@
 "use strict";
 
+const ospiti = [
+    {
+        nome : "Brad Pitt",
+        numPosto : "1",
+        Tavolo : "Tavolo Vip"
+    },
+    {
+        nome : "Johnny Depp",
+        numPosto : "2",
+        Tavolo : "Tavolo Vip"
+    },
+    {
+        nome : "Lady Gaga",
+        numPosto : "3",
+        Tavolo : "Tavolo Vip"
+    },
+    {
+        nome : "Cristiano Ronaldo",
+        numPosto : "4",
+        Tavolo : "Tavolo Vip"
+    },
+    {
+        nome : "Georgina Rodriguez",
+        numPosto : "5",
+        Tavolo : "Tavolo Vip"
+    },
+    {
+        nome : "Chiara Ferragni",
+        numPosto : "6",
+        Tavolo : "Tavolo Vip"
+    },
+    {
+        nome : "Fedez",
+        numPosto : "7",
+        Tavolo : "Tavolo Vip"
+    },
+    {
+        nome : "Gerorge Cloonney",
+        numPosto : "8",
+        Tavolo : "Tavolo Vip"
+    },
+    {
+        nome : "Amal Clooney",
+        numPosto : "9",
+        Tavolo : "Tavolo Vip"
+    },
+    {
+        nome : "Maneskin",
+        numPosto : "10",
+        Tavolo : "Tavolo Vip"
+    }
+]
+
