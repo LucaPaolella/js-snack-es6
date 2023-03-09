1606a8a65bdf91e10a0180269729fac4e5e35fd0
Your Name <you@example.com>
2023-03-09 16:50:56 +0100

    fine snack 3

--- snack3/js/main.js
+++ snack3/js/main.js
@@ -1,1 +1,31 @@
-"use strict"
+"use strict";
+
+const listaBici = [
+    {
+        nome: "rossa",
+        pesoInKg: 5
+    },
+    {
+        nome: "verde",
+        pesoInKg: 4
+    },
+    {
+        nome: "gialla",
+        pesoInKg: 8
+    }
+];
+
+//console.log(listaBici)
+
+
+const pesoBici = listaBici.map((element) => {
+    //console.log(element.pesoInKg);
+    return element.pesoInKg;
+});
+console.log(pesoBici)
+
+console.log(Math.min(...pesoBici));
+
+console.log(`${Math.min(...pesoBici)}`)
+
+
