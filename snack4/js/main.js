832c2d6b165cdee7f6f44c77bae58253b718629a
Your Name <you@example.com>
2023-03-09 17:06:27 +0100

    divido in step lo snack4: step 1 e step 2

--- snack4/js/main.js
+++ snack4/js/main.js
@@ -1,1 +1,26 @@
 "use strict";
+
+//step 1 e 2
+
+const team = [
+    {
+        nome : "Tivoli",
+        punti : "0",
+        falli : "0"
+    },
+    {
+        nome : "Villa Adriana",
+        punti : "0",
+        falli : "0"
+    },
+    {
+        nome : "Villanova",
+        punti : "0",
+        falli : "0"
+    },
+    {
+        nome : "Castel Madama",
+        punti : "0",
+        falli : "0"
+    },
+]
