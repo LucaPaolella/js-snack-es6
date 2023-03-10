dacf77337e03743ff1be4d0f6d55c3f0da718d81
Your Name <you@example.com>
2023-03-10 10:10:46 +0100

    Soluzione alternativa dopo la correzione snack 3

--- snack3/js/main.js
+++ snack3/js/main.js
@@ -18,14 +18,35 @@ const listaBici = [
 //console.log(listaBici)
 
 
-const pesoBici = listaBici.map((element) => {
+//const pesoBici = listaBici.map((element) => {
     //console.log(element.pesoInKg);
-    return element.pesoInKg;
+    /*return element.pesoInKg;
 });
 console.log(pesoBici)
 
 console.log(Math.min(...pesoBici));
 
-console.log(`${Math.min(...pesoBici)}`)
+console.log(`${Math.min(...pesoBici)}`)*/
+
+//Altra strada dopo la lezione 
+
+if (listaBici.length === 0){
+    console.log (`Non ho bici`);
+}
+else {
+    let theBike = listaBici [0];
+    console.log(theBike)
+
+    for (let i=1; i<listaBici.length;i++){
+        const currentBike = listaBici[i];
+
+        if(currentBike.pesoInKg < theBike.pesoInKg){
+            theBike= currentBike
+        }
+        console.log(currentBike)
+    }
+    const {nome, pesoInKg} = theBike
+    console.log(`La bici più leggera è la bici ${nome} che pesa ${pesoInKg} kg`)
+}
 
 
